fda5c2e65ea14b9a8cf6b2703f2debcfc210fc36
Administrator <Administrator@Win11-2022PZNYP>
2024-05-29 23:35:18 +0200

    添加项目文件。

---
+++ School Paper/Data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Paper
+
+{
+    public class Data
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public string Profession { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+
+
+
+    }
+}
